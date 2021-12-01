67da831ef9eeded9efb85bd1229fe9b390112cf6
joao <joao_bortotti@hotmail.com>
2021-11-30 22:05:40 -0300

    arquivos de configuracao

---
+++ config/mail.js
@@ -0,0 +1,11 @@
+mail = {
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: 'joao.bortotti@uotz.com.br',
+    pass: 'ascvasdvb10-sA90d',
+  }
+}
+
+module.exports = mail;
---
+++ config/redis.js
@@ -0,0 +1,5 @@
+redis = {
+  url: "redis://127.0.0.1:6379"
+}
+
+module.exports = redis;
